bd68f6120bbcf7975d716921babc9b5dc8be4bc4
matvei5557 <147399535+matvei5557@users.noreply.github.com>
2023-10-27 17:50:50 +0300

    Update game.py

--- game.py
+++ game.py
@@ -18,7 +18,9 @@ def user_name(surf,text,x,y,size):
     text_rect = text_surfase.get_rect()
     text_rect.midtop = (x,y)
     surf.blit(text_surfase, text_rect)
-while 1:
+bg = pg.image.load('C:/Users/Ученик_7/Downloads/bg_name.jpg')
+main_loop = True
+while main_loop:
     for i in pg.event.get():
         if i.type == pg.QUIT:
             exit()
@@ -26,10 +28,27 @@ while 1:
              if i.key == pg.K_BACKSPACE:
                 name = name[:-1]
              elif i.key == pg.K_RETURN:
-                 print(name)
+                 main_loop = False
              else:
                  name += i.unicode
     win.fill((0,0,0))
+    win.blit(bg,(0,0))
     draw_text(win, 'Введите имя:', W//2,H//2)
     draw_text(win,name,W//2,H//2 + 20)
+    pg.display.update()
+
+gb = pg.image.load('C:/Users/Ученик_7/Downloads/bg_name.pg.png')
+while 1:
+    for i in pg.event.get():
+        if i.type == pg.QUIT:
+            exit()
+
+    win.fill((0,0,0))
+    win.blit(gb,(0,0))
+    for y in range(0,W,35):
+        pg.draw.line(win,(0,0,0), (0,y),(W,y))
+        pg.draw.line(win, (0,0,0), (0, 0), (0, H))
+    for x in range(0,W,35):
+        pg.draw.line(win,(0,0,0), (0,x),(W,x))
+        pg.draw.line(win, (0,0,0), (x, 0), (x, H))
     pg.display.update()
