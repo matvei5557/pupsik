213e00e33c9222f35bf371fe675ab8555055ba6d
matvei5557 <147399535+matvei5557@users.noreply.github.com>
2023-11-20 15:50:57 +0300

    Add files via upload

--- sql_bd.py
+++ sql_bd.py
@@ -1,27 +1,32 @@
 import sqlite3
 
 class DateBaseSQL():
-    db_name: str =  'SQL.db'
-    def __init__(self, name = None):
-        self.con = sqlite3.connect(name or self.db_name)
-        self.cur = self.con.cursor()
-        self.create_table()
+	db_name: str =  'SQL.db'
+	def __init__(self, name = None):
+		self.con = sqlite3.connect(name or self.db_name)
+		self.cur = self.con.cursor()
+		self.create_table()
 
-    def set(self, name, score):
-        db_score = self.get(name=name)
-        if score is not None or score > db_score:
-            self.cur_execute(f"DELETE FROM stock WHERE name='{name}'")
-            self.cur.execute(f"INSERT INTO stocks VALUES ('{name}', {score})")
-            self.con.commit()
-    def get(self, name=None, limit=5):
-        if name:
-            rows = self.cur.execute(f'SELET score FROM stocks WHERE name="{name}" ODEER EY score')
-            rows = list(rows)
-            return  rows[0][0] if rows else None
+	def set(self, name, score):
+		db_score = self.get(name=name)
+		if score is not None or score > db_score:
+			self.cur.execute(f"DELETE FROM stocks WHERE name='{name}'")
+			self.cur.execute(f"INSERT INTO stocks VALUES ('{name}', {score})")
+			self.con.commit()
 
-        scores = list(self.cur.execute(f"SELECT * FROM stocks ORDER BY score DESC LIMIT {limit}"))
-        return scores
+	def get(self, name=None, limit=5):
+		if name:
+			# SELECT <col1, col2, ...> FROM <table_name> where <condition>
+			rows = self.cur.execute(f'SELECT score FROM stocks WHERE name="{name}" ORDER BY score')
+			rows = list(rows)
+			# getting first result and first position of the tuple
+			return rows[0][0] if rows else None
 
-    def create_tabele(self):
-        self.cur.execute('''CREATE TABLE IF NOT EXISTS stocks (name text, score init)''')
-        self.con.commit()
+		scores = list(self.cur.execute(f'SELECT * FROM stocks ORDER BY score DESC LIMIT {limit}'))
+		return scores
+
+	def create_table(self):
+		self.cur.execute('''CREATE TABLE IF NOT EXISTS stocks (name text, score int)''')
+		self.con.commit()
+
+
